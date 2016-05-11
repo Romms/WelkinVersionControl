2177f0c822926c5b375641a092e7a9e959597bfb
Roman Shevchuk <rommssh@gmail.com>
2016-05-11 19:59:29 +0300

    Start!

---
+++ WelkinVersionControl.ps1
@@ -0,0 +1,21 @@
+cd ${env:ProgramFiles(x86)}
+cd "The Welkin Suite"
+
+$currentVersion = (Get-Item "The Welkin Suite\TheWelkinSuite.exe").VersionInfo.ProductVersion
+$copyName = "The Welkin Suite $currentVersion"
+
+$copyExists = Test-Path $copyName
+
+if (!$copyExists) {
+	# Copy
+	New-Item "The Welkin Suite $currentVersion" -type directory
+	Copy-Item "The Welkin Suite\*" "The Welkin Suite $currentVersion"
+	
+	# Create Shortcut
+	
+	$desktopPath = [Environment]::GetFolderPath("CommonDesktopDirectory")
+	$WScriptShell = New-Object -ComObject WScript.Shell
+	$Shortcut = $WScriptShell.CreateShortcut("$desktopPath\$copyName.lnk")
+	$Shortcut.TargetPath = "$pwd\$copyName\TheWelkinSuite.exe"
+	$Shortcut.Save()
+}
