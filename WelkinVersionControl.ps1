34802bd5f435b5177bd0f7c44106454c9628013c
Roman Shevchuk <rommssh@gmail.com>
2016-05-12 18:49:38 +0300

    Directory depth

--- WelkinVersionControl.ps1
+++ WelkinVersionControl.ps1
@@ -1,7 +1,7 @@
 cd ${env:ProgramFiles(x86)}
-cd "The Welkin Suite"
+#cd "The Welkin Suite"
 
-$currentVersion = (Get-Item "The Welkin Suite\TheWelkinSuite.exe").VersionInfo.ProductVersion
+$currentVersion = (Get-Item "The Welkin Suite\The Welkin Suite\TheWelkinSuite.exe").VersionInfo.ProductVersion
 $copyName = "The Welkin Suite $currentVersion"
 
 $copyExists = Test-Path $copyName
@@ -9,13 +9,12 @@ $copyExists = Test-Path $copyName
 if (!$copyExists) {
 	# Copy
 	New-Item "The Welkin Suite $currentVersion" -type directory
-	Copy-Item "The Welkin Suite\*" "The Welkin Suite $currentVersion"
+	Copy-Item "The Welkin Suite\The Welkin Suite" "The Welkin Suite $currentVersion\"
 	
 	# Create Shortcut
-	
 	$desktopPath = [Environment]::GetFolderPath("CommonDesktopDirectory")
 	$WScriptShell = New-Object -ComObject WScript.Shell
 	$Shortcut = $WScriptShell.CreateShortcut("$desktopPath\$copyName.lnk")
-	$Shortcut.TargetPath = "$pwd\$copyName\TheWelkinSuite.exe"
+	$Shortcut.TargetPath = "$pwd\$copyName\The Welkin Suite\TheWelkinSuite.exe"
 	$Shortcut.Save()
 }
