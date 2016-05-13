00555cdd3a9629a7fe928c53a4a6259098d254f2
Roman Shevchuk <rommssh@gmail.com>
2016-05-13 11:38:49 +0300

    Formating changed

--- WelkinVersionControl.ps1
+++ WelkinVersionControl.ps1
@@ -1,8 +1,8 @@
 param([switch]$Elevated)
 
 function Test-Admin {
-  $currentUser = New-Object Security.Principal.WindowsPrincipal $([Security.Principal.WindowsIdentity]::GetCurrent())
-  $currentUser.IsInRole([Security.Principal.WindowsBuiltinRole]::Administrator)
+	$currentUser = New-Object Security.Principal.WindowsPrincipal $([Security.Principal.WindowsIdentity]::GetCurrent())
+	$currentUser.IsInRole([Security.Principal.WindowsBuiltinRole]::Administrator)
 }
 
 if ((Test-Admin) -eq $false)  {
@@ -10,16 +10,16 @@ if ((Test-Admin) -eq $false)  {
         # tried to elevate, did not work, aborting
     } 
     else {
-		Set-ExecutionPolicy Unrestricted -Force
-        Start-Process powershell.exe -Verb RunAs -ArgumentList ('-noprofile -noexit -file "{0}" -elevated' -f ($myinvocation.MyCommand.Definition))
-	}
+        #Try to run as administrator
+        #Set-ExecutionPolicy Unrestricted -Force
+        Start-Process powershell.exe -Verb RunAs -ArgumentList ('-noprofile -file "{0}" -elevated' -f ($myinvocation.MyCommand.Definition))
+    }
 
-	exit
+    exit
 }
 
-
+# Main 
 cd ${env:ProgramFiles(x86)}
-#cd "The Welkin Suite"
 
 $currentVersion = (Get-Item "The Welkin Suite\The Welkin Suite\TheWelkinSuite.exe").VersionInfo.ProductVersion
 $copyName = "The Welkin Suite $currentVersion"
@@ -27,14 +27,16 @@ $copyName = "The Welkin Suite $currentVersion"
 $copyExists = Test-Path $copyName
 
 if (!$copyExists) {
-	# Copy
-	New-Item "The Welkin Suite $currentVersion" -type directory
-	Copy-Item "The Welkin Suite\The Welkin Suite" "The Welkin Suite $currentVersion\" -recurse 
-	
-	# Create Shortcut
-	$desktopPath = [Environment]::GetFolderPath("CommonDesktopDirectory")
-	$WScriptShell = New-Object -ComObject WScript.Shell
-	$Shortcut = $WScriptShell.CreateShortcut("$desktopPath\$copyName.lnk")
-	$Shortcut.TargetPath = "$pwd\$copyName\The Welkin Suite\TheWelkinSuite.exe"
-	$Shortcut.Save()
+    # Create directory for new copy
+    #New-Item "The Welkin Suite $currentVersion" -type directory
+    New-Item "The Welkin Suite $currentVersion\The Welkin Suite" -type directory
+    # Copy to new directory
+    Copy-Item "The Welkin Suite\The Welkin Suite\*" "The Welkin Suite $currentVersion\The Welkin Suite\"
+    
+    # Create shortcut on desktop
+    $desktopPath = [Environment]::GetFolderPath("CommonDesktopDirectory")
+    $WScriptShell = New-Object -ComObject WScript.Shell
+    $Shortcut = $WScriptShell.CreateShortcut("$desktopPath\$copyName.lnk")
+    $Shortcut.TargetPath = "$pwd\$copyName\The Welkin Suite\TheWelkinSuite.exe"
+    $Shortcut.Save()
 }
